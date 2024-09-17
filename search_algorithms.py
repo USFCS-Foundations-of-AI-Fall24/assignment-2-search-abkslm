81e593f19ec2c3f9c44b794e735bcab5a25223cc
A. Blakeslee Moore <andrew@abkslm.com>
2024-09-17 16:08:41 -0700

    added depth-limiting to DFS

--- search_algorithms.py
+++ search_algorithms.py
@@ -43,28 +43,33 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
     search_queue = deque()
     closed_list = {}
 
-    search_queue.append((startState,""))
+    search_queue.append((startState, "", 0))
     if use_closed_list :
         closed_list[startState] = True
     while len(search_queue) > 0 :
         ## this is a (state, "action") tuple
         next_state = search_queue.pop()
-        if goal_test(next_state[0]):
+        state, action, depth = next_state
+        if goal_test(state):
             print("Goal found")
             print(next_state)
-            ptr = next_state[0]
+            ptr = state
             while ptr is not None :
                 ptr = ptr.prev
                 print(ptr)
             return next_state
-        else :
-            successors = next_state[0].successors(action_list)
+        elif depth < limit or limit == 0:
+            successors = state.successors(action_list)
             if use_closed_list :
                 successors = [item for item in successors
                                     if item[0] not in closed_list]
                 for s in successors :
                     closed_list[s[0]] = True
-            search_queue.extend(successors)
+            # search_queue.extend(successors)
+            search_queue.extend((s[0], s[1], depth + 1) for s in successors)
+
+    print(f"Goal not found within limit: {limit}")
+    return None
 
 ## add iterative deepening search here
 
