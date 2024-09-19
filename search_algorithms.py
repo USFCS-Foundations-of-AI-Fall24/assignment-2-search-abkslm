24196472d2024eae515316027bad664414c8b6f7
A. Blakeslee Moore <andrew@abkslm.com>
2024-09-19 12:22:01 -0700

    Modified states_generated:int logic.

--- search_algorithms.py
+++ search_algorithms.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 
-
 ## We will append tuples (state, "action") in the search queue
 def breadth_first_search(startState, action_list, goal_test, use_closed_list=True) :
     search_queue = deque()
     closed_list = {}
 
-    states_generated = 0
-
     search_queue.append((startState,""))
+
+    states_generated = 1
+
     if use_closed_list :
         closed_list[startState] = True
     while len(search_queue) > 0 :
@@ -20,13 +20,12 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
 
         if goal_test(next_state[0]):
             print("Goal found")
-            print(next_state)
             ptr = next_state[0]
             while ptr is not None :
                 ptr = ptr.prev
                 print(ptr)
             print(f"\nStates generated: {states_generated}\n")
-            return next_state
+            return next_state[0]
         else :
             successors = next_state[0].successors(action_list)
             if use_closed_list :
@@ -43,9 +42,10 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
     search_queue = deque()
     closed_list = {}
 
-    states_generated = 0
-
     search_queue.append((startState, "", 0))
+
+    states_generated = 1
+
     if use_closed_list :
         closed_list[startState] = True
     while len(search_queue) > 0 :
